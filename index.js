94f5ccb5a7d920967aed67f8e19ea8094ddfef62
username <tuandangvann@gmail.com>
2023-11-24 20:15:06 +0700

    push

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Express on Vercel 2");
 });
 
 app.listen(5000, () => {
