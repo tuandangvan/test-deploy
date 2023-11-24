16df50a11f3f3709cb29c492bedf6ad21a5cc2be
username <tuandangvann@gmail.com>
2023-11-24 19:59:30 +0700

    push

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import express from "express";
 import { APIs_V1 } from "./src/routes/v1/index";
 import cors from "cors";
 
-const START_SERVER = () => {
+
   const app = express();
 
-  // // Enable req.body json data
-  // app.use(express.json());
-  // app.use(cors());
+  // Enable req.body json data
+  app.use(express.json());
+  app.use(cors());
 
-  // //Use APIs v1
-  // app.use("/api/v1", APIs_V1);
+  //Use APIs v1
+  app.use("/api/v1", APIs_V1);
 
   app.get("/", (req, res) => {
     res.send("Express on Vercel");
@@ -20,6 +20,6 @@ const START_SERVER = () => {
 
   const server = app.listen(5000, () => {
   });
-};
 
-START_SERVER();
+  module.exports = app;
+
